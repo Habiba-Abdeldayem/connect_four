38d6e127e36fe17c7d509e5c1ca70fc6e27ad5e3
Habiba <habiba5543@live.com>
2022-08-18 20:45:27 +0200

    set tag method added

--- app/src/main/java/com/example/connect_four/MainActivity.java
+++ app/src/main/java/com/example/connect_four/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button[][] buttons = new Button[4][4];
 
-    private boolean player1Turn;
+    private boolean player1Turn = true;
 
     private int roundCount;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int greyBtn_id;
     private int blueBtn_id;
     private int redBtn_id;
+    private String greyBtn_background;
 
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer2 = (TextView) findViewById(R.id.p2_textView);
         playAgain = (Button)findViewById(R.id.button_playAgain);
         home = (Button)findViewById(R.id.button_home);
+        setButtons();
     }
 
 
@@ -53,7 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[i][j] = findViewById(R_buttonId);
                 // logic is written in onClick() , we had also to implement View.OnClickListener
                 buttons[i][j].setOnClickListener(this);
-
+                greyBtn_background = findViewById(R.id.imageButton00).getResources().toString();
+                buttons[i][j].setTag("0"); // 1 for blue, 2 for red, 0 for grey
 
             }
         }
@@ -64,44 +68,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         ImageButton clicked_button=(ImageButton)view;
-        greyBtn_id = R.drawable.grey_btn;
-        blueBtn_id = R.drawable.blue_btn;
-        redBtn_id = R.drawable.red_btn;
+
+        if(!(clicked_button.getTag().equals("0"))) {
+            return;
+        }
+
+        if(player1Turn){
+            clicked_button.setTag("1");
+            clicked_button.setBackgroundResource(R.drawable.blue_btn);
+            Log.i("blue","i'm in the blue");
+        }
+        else{
+            clicked_button.setTag("2");
+            clicked_button.setBackgroundResource(R.drawable.red_btn);
+        }
+
+        roundCount++;
+
+        if(checkForWin()){
+            if(player1Turn){
+                player1wins();
+            }
+            else{
+                player2wins();
+            }
+        }
+        else if(roundCount==9){
+            draw();
+        }
+        else{
+            player1Turn = !player1Turn;
+        }
+
 
     }
+
+
     private boolean checkForWin() {
 
         String[][] field = new String[4][4];
 
-
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                field[i][j] = buttons[i][j].getBackground().toString();
+                field[i][j] = buttons[i][j].getTag().toString();
             }
 
         }
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (field[i][0].equals(field[i][1])&&field[i][0].equals(field[i][2])&&!field[i][0].equals("#BDC4C5"))
+                if (field[i][0].equals(field[i][1])&&field[i][0].equals(field[i][2])&&!field[i][0].equals(greyBtn_background))
                 {
                     return true;
                 }
-                else if (field[0][i].equals(field[1][i])&&field[0][i].equals(field[2][i])&&!field[0][i].equals("#BDC4C5")) {
+                else if (field[0][i].equals(field[1][i])&&field[0][i].equals(field[2][i])&&!field[0][i].equals(greyBtn_background)) {
                     return true;
                 }
-                else if (field[0][0].equals(field[1][1])&&field[0][0].equals(field[2][2])&&field[0][0].equals(field[3][3])&&!field[0][0].equals("#BDC4C5"))
+                else if (field[0][0].equals(field[1][1])&&field[0][0].equals(field[2][2])&&field[0][0].equals(field[3][3])&&!field[0][0].equals(greyBtn_background))
                 {
                     return true;
                 }
-                else if (field[3][0].equals(field[2][1])&&field[3][0].equals(field[1][2])&&field[3][0].equals(field[0][3])&&!field[3][0].equals("#BDC4C5"))
+                else if (field[3][0].equals(field[2][1])&&field[3][0].equals(field[1][2])&&field[3][0].equals(field[0][3])&&!field[3][0].equals(greyBtn_background))
                 {
                     return true;
                 }
-                return false;
+
 
             }
 
         }
+        return false;
+    }
 
+
+    private void player1wins() {
+    }
+    private void player2wins() {
+    }
+    private void draw() {
     }
 }
